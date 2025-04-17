8c39634a103ec612028ae00956676d00764785d6
ktung <ptung@protonmail.ch>
2025-04-17 16:51:20 -0400

    use layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Footer from '$lib/components/footer.svelte';
+  import { injectAnalytics } from '@vercel/analytics/sveltekit';
+  import { injectSpeedInsights } from '@vercel/speed-insights';
+
+  let { children } = $props();
+
+  injectAnalytics();
+  injectSpeedInsights();
+</script>
+
+<main>
+  <div>
+    {@render children()}
+  </div>
+
+  <Footer />
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,6 @@
 <script>
   import Footer from '$lib/components/footer.svelte';
-  import { injectAnalytics } from '@vercel/analytics/sveltekit';
-  import { injectSpeedInsights } from '@vercel/speed-insights';
-
-  injectAnalytics();
-  injectSpeedInsights();
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-<Footer />
