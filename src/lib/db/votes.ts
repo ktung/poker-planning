8a944e0bbf62018ee3cec875aa65030550f80327
ktung <ptung@protonmail.ch>
2025-04-19 19:25:30 -0400

    fix: fix upsert votes

--- src/lib/db/votes.ts
+++ src/lib/db/votes.ts
@@ -6,12 +6,21 @@ export const upsertVote = async (
   type: 'complexity' | 'effort' | 'uncertainty',
   value: number | null
 ) => {
+  const { data: userId } = await supabase
+    .from('users')
+    .select('id')
+    .eq('session_id', sessionId)
+    .single();
+  if (!userId || !userId.id) {
+    throw new Error('User not found with session_id ' + sessionId);
+  }
+
   return supabase
     .from('votes')
     .upsert(
       {
         room_id: roomId,
-        user_id: sessionId,
+        user_id: userId.id,
         type: type,
         value: value
       },
