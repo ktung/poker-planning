3dcf4d4f80f641a24921ad80e7102f78783b9dad
ktung <ptung@protonmail.ch>
2025-04-19 18:57:16 -0400

    fix: fix cron job naming

--- supabase/migrations/20250418225321_cron_cleanup.sql
+++ supabase/migrations/20250418225321_cron_cleanup.sql
@@ -4,7 +4,7 @@ grant usage on schema cron to postgres;
 grant all privileges on all tables in schema cron to postgres;
 
 select cron.schedule (
-  'everyday_cleaup', -- name of the cron job
+  'everyday_cleanup', -- name of the cron job
   '30 5 * * *', -- Everyday at 5:30AM (GMT)
   $$ delete from rooms where updated_at < now() - interval '1 day' $$
 );
