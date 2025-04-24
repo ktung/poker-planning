fe20ef5f630c71eabf96b71a21469bfe1a307f75
ktung <ptung@protonmail.ch>
2025-04-24 18:38:33 -0400

    refactor: add vote type

--- src/lib/db/votes.ts
+++ src/lib/db/votes.ts
@@ -1,7 +1,15 @@
 import { supabase } from '$lib/supabaseClient';
 
+interface Vote {
+  room_id: string;
+  user_id: string;
+  complexity?: number | null;
+  effort?: number | null;
+  uncertainty?: number | null;
+}
+
 export const upsertVote = async (userId: string, roomId: string, type: VoteType, value: number | null) => {
-  const data = {
+  const data: Vote = {
     room_id: roomId,
     user_id: userId
   };
