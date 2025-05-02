011a24a4c4498c66abf18da9040dd792b75c4b1a
ktung <ptung@protonmail.ch>
2025-05-01 23:34:20 -0400

    feat: improve gurd to redirect to room join link

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,6 @@
 import { redirect, type Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
+import { fetchRoom } from '$lib/db/rooms';
 import { paraglideMiddleware } from '$lib/paraglide/server';
 import { logger } from '$lib/util/logger';
 import { ROOM_ID_ALPHABET, ROOM_ID_LENGTH } from '$lib/util/room';
@@ -18,12 +19,17 @@ const handleRoom: Handle = async ({ event, resolve }) => {
   const roomPathRegex = new RegExp(`^/[${ROOM_ID_ALPHABET}]{${ROOM_ID_LENGTH}}$`);
   if (roomPathRegex.test(event.url.pathname)) {
     const referer = event.request.headers.get('referer');
-    if (!referer) {
-      logger.error('referer null', referer);
-      throw redirect(303, '/');
-    }
-    if (referer !== `${event.url.origin}/` && referer !== `${getJoinUrl(event.url.href)}`) {
-      logger.debug('referer', referer);
+    if (!referer || (referer !== `${event.url.origin}/` && referer !== `${getJoinUrl(event.url.href)}`)) {
+      logger.error('referer', referer);
+
+      const roomSlug = event.params.slug;
+      if (roomSlug) {
+        const { error } = await fetchRoom(roomSlug);
+        if (!error) {
+          throw redirect(303, getJoinUrl(`${event.url.origin}/${roomSlug}`));
+        }
+      }
+
       throw redirect(303, '/');
     }
   }
