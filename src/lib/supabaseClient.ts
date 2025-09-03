f7faf8e108d84b31909aa6bbd8400b455b3aed11
ktung <ptung@protonmail.ch>
2025-09-03 09:59:18 -0400

    feat: use supabase realtime with worker to keep the connection active

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,4 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 import type { Database } from './db/database.types';
 
-export const supabase = createClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
+export const supabase = createClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+  realtime: {
+    worker: true
+  }
+});
