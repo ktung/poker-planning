b5e8821b366e77f4ad5200092b6107e06bd6d171
ktung <ptung@protonmail.ch>
2025-11-13 17:32:12 -0500

    redirect to join

--- src/routes/[slug=nanoid]/+page.server.ts
+++ src/routes/[slug=nanoid]/+page.server.ts
@@ -1,4 +1,5 @@
-import { error } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
+import { resolve } from '$app/paths';
 import { fetchVotesAndUsersByRoomId } from '$lib/remote/votes.remote';
 import { fetchRoom } from '$lib/server/db/rooms';
 import { findUser } from '$lib/server/db/users';
@@ -7,17 +8,21 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params, cookies }) => {
   const slug = params.slug;
-  const userId = cookies.get('userId');
 
-  if (!slug || !userId) {
-    logger.error('Missing slug or userId');
-    throw new Error('Missing slug or userId');
+  if (!slug) {
+    logger.error('Missing slug');
+    throw new Error('Missing slug');
   }
 
   const { data: room, error: fetchError } = await fetchRoom(slug);
   if (fetchError || !room) {
     logger.error('Error fetching room', error);
     error(404, 'Error fetching room');
+  }
+
+  const userId = cookies.get('userId');
+  if (!userId) {
+    redirect(303, resolve('/[slug=nanoid]/join', { slug: slug }));
   }
 
   const { data: currentUser, error: userError } = await findUser(userId);
