c3c55998325d4aed8bfe7734be82575d5bdd05ce
ktung <ptung@protonmail.ch>
2026-01-16 23:17:39 -0500

    chore: update deps

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,10 @@
-import adapterAuto from '@sveltejs/adapter-auto';
 import adapterVercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-let customAdapter;
-if (process.env.ADAPTER === 'vercel') {
-  customAdapter = adapterVercel();
-} else {
-  customAdapter = adapterAuto();
-}
 
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: customAdapter,
+    adapter: adapterVercel(),
     experimental: {
       remoteFunctions: true
     }
